f3a4cdb1119d394b5e57d254a1b4382a1906d735
Gerardo Valencia <gerardovalencia@Huriatas-MacBook-Pro.local>
2023-06-02 13:17:37 -0600

    add max logo to the layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,12 +4,9 @@ export default function Layout() {
   return (
     <div className="px-4">
       <header className="flex flex-row items-center justify-between">
-        <div className="flex flex-col">
-          <h1 className="m-0">Max</h1>
-          <small className="font-light uppercase">
-            Music Audience Exchange
-          </small>
-        </div>
+        <Link to="/">
+          <img src="../../../public/max-logo.webp" alt="Max" className="w-44" />
+        </Link>
         <Link to="/my-list">View My List</Link>
       </header>
       <main className="my-8">
